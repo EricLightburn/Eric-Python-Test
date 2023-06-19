b3c39d989e2277ce19d4ea1091ffba6f7e98ef4e
EricLightburn <ericlightburn12@gmail.com>
2023-06-19 11:01:28 +0100

    command line (1-100 guess game)

---
+++ main.py
@@ -0,0 +1,13 @@
+from random import randint
+guess = 0
+tries = 0
+number = randint(1,100)
+while (guess!=number):
+    guess = int(input("Choose a number between 1 and 100: ")) 
+    if (guess>number):
+            print("lower")
+    elif (guess<number):
+            print("higher")
+    tries = tries + 1
+
+print("It took you "+ str(tries) + " tries to guess the")
