46680502343615940e07f89540c7ecc793202a54
EricLightburn <ericlightburn12@gmail.com>
2023-06-20 13:50:41 +0100

    number of letters finish

--- Number of letters in word.py
+++
@@ -1,5 +0,0 @@
-from collections import Counter
-my_file = open(r"c:\Users\training.user\Downloads\wordz\words.txt", "r")
-counter = Counter(my_file.read())
-counter['a']
-print("NUMBER OF LETTERS FROM MOST TO LEAST: \n \n \n \n \n" + str(counter))
---
+++ number_of_letters.py
@@ -0,0 +1,15 @@
+import string
+import collections
+with open(r"c:\Users\training.user\Documents\words.txt", "r") as f:
+    words = f.read().split()
+letter_counts = {}
+for word in words:
+    word = word.lower()
+    for letter in word:
+        if letter not in letter_counts:
+            letter_counts[letter] = 0
+        letter_counts[letter] += 1
+sorted_letter_counts = sorted(letter_counts.items(), key=lambda x: x[0])
+for letter, count in sorted_letter_counts:
+#(found online, gets rid of non letters)if letter not in string.punctuation and letter not in string.digits: 
+        print(f"{letter}: {count}")
