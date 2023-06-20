f38466ef0c31c12977d5a6af82a8a13ab1339f48
EricLightburn <ericlightburn12@gmail.com>
2023-06-20 11:16:06 +0100

    BANK

--- Bank_Account but a bit better.py
+++ Bank_Account but a bit better.py
@@ -15,15 +15,19 @@ class Bank_Account:
 		else:
 			self.balance -= amount
 			print("\n You Withdrew: ", amount)
-			year = float(input("How many years have you left your account?: "))
-			interest = 0.05
-			compound_final = self.balance * ((1 + interest / 1) ** year)
-			self.balance = compound_final
-			print("\n You have left your account for: ", year, " years and you have a ") 
-			print("\n Net Available Balance of",self.balance)
+	
+	def increment_year(self):		
+		year = float(input("How many years have you left your account?: "))
+		interest = 0.05
+		compound_final = self.balance * ((1 + interest / 1) ** year)
+		self.balance = compound_final
+		print("\n You have left your account for: ", year, " years and you have a ") 
+		print("\n Net Available Balance of",self.balance)
 
 s = Bank_Account()
 
+s.__init__()
 s.deposit()
 s.withdraw()
+s.increment_year()
 
