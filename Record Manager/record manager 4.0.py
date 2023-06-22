04703e720145289660ed14fed6a744b9baa72e79
EricLightburn <ericlightburn12@gmail.com>
2023-06-22 10:53:48 +0100

    Finish other records and visuals

--- Record Manager/record manager 4.0.py
+++ Record Manager/record manager 4.0.py
@@ -32,7 +32,7 @@ class RecordManager:
         record = Record(name, age, interests)
         self.records.append(record)
         with open(self.file_path, "a") as writeRecord:
-            writeRecord.write(f"\t\t\t{name}\t\t\t{age}\t\t\t{interests}\n")
+            writeRecord.write(f"\t\t\t{name}\t\t\t\t{age}\t\t\t\t{interests}\n")
     def view_records(self):
         if len(self.records) == 0:
             print("There are no records")
@@ -40,3 +40,33 @@ class RecordManager:
         else:
             with open(self.file_path, "r") as readRecords:
                 print(readRecords.read())
+    def delete_record(self):
+        record_id = int(input("Enter Record #: ")) - 1
+        if 0 <= record_id < len(self.records):
+            del self.records[record_id]
+            with open(self.file_path, "w") as readRecords2:
+                readRecords2.write("#\t\t\tName\t\t\tAge\t\t\tInterests\n")
+                for record in self.records:
+                    readRecords2.write(f"\t\t\t{record.name}\t\t\t{record.age}\t\t\t{record.interests}\n")
+            print("Record Deleted.")
+        else:
+            print("Invalid record number.")
+    def clear_records(self):
+        self.records = []
+        with open(self.file_path, "w") as f:
+            f.truncate(0)
+            f.write("#\t\t\tName\t\t\tAge\t\t\tInterests\t\t\t")
+    def find_records(self, name):
+        with open(self.file_path, "r") as f:
+            for line in f:
+                if name in line:
+                    print(line)
+print("==========================================================")
+print(" ~ ----------------- Record Manager --------------------- ~")
+print("===========================================================")
+print("Available Operators:")
+print("A) Add Record\t\tB) View Record")
+print("C) Delete record\tD) Clear Records")
+print("E) Exit manager\t\tF) Find Records \n")
+
+selection = input('Enter selection: ').upper()      
