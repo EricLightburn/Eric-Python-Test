86f3e8326cae7e582bc77b04386c801a2a3e2001
EricLightburn <ericlightburn12@gmail.com>
2023-06-22 11:29:48 +0100

    Fix Find option and Add selection

--- Record Manager/record manager 4.0.py
+++ Record Manager/record manager 4.0.py
@@ -14,17 +14,16 @@ class RecordManager:
         self.records = []
         self.load_records()
     def load_records(self):
-            if not Path(self.file_path).exists():
-                self.create_file()
-            with open(self.file_path, "r") as records:
-                for line in records.readlines()[1:]:
-                    fields = line.strip().split("\t")
-                    record = Record(fields[1], fields[2], fields[3])
-                    self.records.append(record)
-
+        if not Path(self.file_path).exists():
+            self.create_file()
+        with open(self.file_path, "r") as records:
+            for line in records.readlines()[1:]:
+                fields = line.strip().split("\t")
+                record = Record(fields[1], fields[2], fields[3])
+                self.records.append(record)
     def create_file(self):
-            with open(self.file_path, "w+") as create:
-                create.write("#\t\t\tName\t\t\tAge\t\t\tInterests")
+        with open(self.file_path, "w+") as create:
+            create.write("#\t\t\tName\t\t\t\t\tAge\t\t\t\tInterests\n")
     def add_record(self):
         name = input("Enter your name: ")
         age = input("Enter your age: ")
@@ -45,9 +44,9 @@ class RecordManager:
         if 0 <= record_id < len(self.records):
             del self.records[record_id]
             with open(self.file_path, "w") as readRecords2:
-                readRecords2.write("#\t\t\tName\t\t\tAge\t\t\tInterests\n")
+                readRecords2.write("#\t\t\tName\t\t\t\tAge\t\t\t\tInterests\n")
                 for record in self.records:
-                    readRecords2.write(f"\t\t\t{record.name}\t\t\t{record.age}\t\t\t{record.interests}\n")
+                    readRecords2.write(f"\t\t\t{record.name}\t\t\t\t{record.age}\t\t\t\t{record.interests}\n")
             print("Record Deleted.")
         else:
             print("Invalid record number.")
@@ -55,12 +54,14 @@ class RecordManager:
         self.records = []
         with open(self.file_path, "w") as f:
             f.truncate(0)
-            f.write("#\t\t\tName\t\t\tAge\t\t\tInterests\t\t\t")
-    def find_records(self, name):
+            f.write("#\t\t\tName\t\t\t\tAge\t\t\t\tInterests\t\t\t\n")
+    def find_records(self):
+        name = input("Enter name to search for: ")
         with open(self.file_path, "r") as f:
             for line in f:
                 if name in line:
                     print(line)
+
 print("==========================================================")
 print(" ~ ----------------- Record Manager --------------------- ~")
 print("===========================================================")
@@ -69,4 +70,21 @@ print("A) Add Record\t\tB) View Record")
 print("C) Delete record\tD) Clear Records")
 print("E) Exit manager\t\tF) Find Records \n")
 
-selection = input('Enter selection: ').upper()      
+manager = RecordManager()
+
+while True:
+    selection = input('Enter selection: ').upper()
+    if selection == 'A':
+        manager.add_record()
+    elif selection == 'B':
+        manager.view_records()
+    elif selection == 'C':
+        manager.delete_record()
+    elif selection == 'D':
+        manager.clear_records()
+    elif selection == 'F':
+        manager.find_records()
+    elif selection == 'E':
+        sys.exit()
+    else:
+        print("Invalid selection. Please try again.")
