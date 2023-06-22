4f11af8f9c8e5b5492d1cca562b106c96899d902
EricLightburn <ericlightburn12@gmail.com>
2023-06-22 09:54:20 +0100

    Change format for easier reading

--- Number of Letters/number_of_letters_2.0.py
+++ Number of Letters/number_of_letters_2.0.py
@@ -6,5 +6,5 @@ with open(r"c:\Users\training.user\Documents\words.txt","r") as file:
     letter_count = Counter(word)  
     sorted_letter_count = dict(sorted(letter_count.items()))
     for letter, count in sorted_letter_count.items():       
-        if letter not in string.punctuation and letter not in string.digits:    #copied from number_of_letters.py
-            print(f'{letter}: {count}')
+        if letter not in string.punctuation and letter not in string.digits:    #copied from 
+            print( f'{letter}:    {count}')                                         #number_of_letters.py
