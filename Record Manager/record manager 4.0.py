399bd30d97c50c89285b2586395e39a0d7940d4b
EricLightburn <ericlightburn12@gmail.com>
2023-06-22 10:17:13 +0100

    Define load_records and create_file

--- Record Manager/record manager 4.0.py
+++ Record Manager/record manager 4.0.py
@@ -13,3 +13,15 @@ class RecordManager:
         self.file_path = r"c:\Users\training.user\Downloads\record.txt"
         self.records = []
         self.load_records()
+    def load_records(self):
+            if not Path(self.file_path).exists():
+                self.create_file()
+            with open(self.file_path, "r") as records:
+                for line in records.readlines()[1:]:
+                    fields = line.strip().split("\t")
+                    record = Record(fields[1], fields[2], fields[3])
+                    self.records.append(record)
+
+    def create_file(self):
+            with open(self.file_path, "w+") as create:
+                create.write("#\t\t\tName\t\t\tAge\t\t\tInterests")
