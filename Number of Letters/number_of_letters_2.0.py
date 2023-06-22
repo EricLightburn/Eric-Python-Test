27699cd7efbb613ef5af0c7f2f295ca7323012f0
EricLightburn <ericlightburn12@gmail.com>
2023-06-22 09:25:07 +0100

    Make Alphabetically and More readable

--- Number of Letters/number_of_letters_2.0.py
+++ Number of Letters/number_of_letters_2.0.py
@@ -3,4 +3,8 @@ from collections import Counter
 with open(r"c:\Users\training.user\Documents\words.txt","r") as file:
     word = file.read()
     letter_count = Counter(word)
-    print(letter_count)
+    word = ', '.join(filter(str.isalpha, word))
+    sorted_letter_count = dict(sorted(letter_count.items()))
+
+    for letter, count in sorted_letter_count.items():
+        print(f'{letter}: {count}')
