fdad4e272fecfc2c7b6a1944fba197d3210c0d88
EricLightburn <ericlightburn12@gmail.com>
2023-06-22 09:42:16 +0100

    Remove join

--- Number of Letters/number_of_letters_2.0.py
+++ Number of Letters/number_of_letters_2.0.py
@@ -3,10 +3,8 @@ import string
 print("\nTotal number of letters is :  ")
 with open(r"c:\Users\training.user\Documents\words.txt","r") as file:
     word = file.read().upper() #to convert so letters are counted as the same wether upper or lower
-    letter_count = Counter(word)
-    word = ', '.join(filter(str.isalpha, word))  
+    letter_count = Counter(word)  
     sorted_letter_count = dict(sorted(letter_count.items()))
-
-    for letter, count in sorted_letter_count.items():
+    for letter, count in sorted_letter_count.items():       
         if letter not in string.punctuation and letter not in string.digits:    #copied from number_of_letters.py
             print(f'{letter}: {count}')
