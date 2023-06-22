e75b5c76449098d1f6b371dc917919a2a6afb99c
EricLightburn <ericlightburn12@gmail.com>
2023-06-22 09:39:15 +0100

    Ignore non letters+count both upper and lower as 1

--- Number of Letters/number_of_letters_2.0.py
+++ Number of Letters/number_of_letters_2.0.py
@@ -1,10 +1,12 @@
 from collections import Counter 
-
+import string
+print("\nTotal number of letters is :  ")
 with open(r"c:\Users\training.user\Documents\words.txt","r") as file:
-    word = file.read()
+    word = file.read().upper() #to convert so letters are counted as the same wether upper or lower
     letter_count = Counter(word)
-    word = ', '.join(filter(str.isalpha, word))
+    word = ', '.join(filter(str.isalpha, word))  
     sorted_letter_count = dict(sorted(letter_count.items()))
 
     for letter, count in sorted_letter_count.items():
-        print(f'{letter}: {count}')
+        if letter not in string.punctuation and letter not in string.digits:    #copied from number_of_letters.py
+            print(f'{letter}: {count}')
