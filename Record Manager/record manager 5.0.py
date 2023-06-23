31c96255d2000eab7ea670ad9a71ba4f4e5ab1c4
EricLightburn <ericlightburn12@gmail.com>
2023-06-23 09:20:46 +0100

    Add __name__ and __main__

--- Record Manager/record manager 5.0.py
+++ Record Manager/record manager 5.0.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 class Color:
     RED = '\033[31m'
     GREEN = '\033[32m'
@@ -15,13 +14,11 @@ class Record:
         self.name = name
         self.age = age
         self.interests = interests
-
 class RecordFile:
     def __init__(self, file_path):
         self.file_path = file_path
         if not os.path.exists(self.file_path):
             self.create_file()
-    
     def create_file(self):
         with open(self.file_path, "w+") as create:
             create.write(f"{Color.BLUE}#\t\t\tName\t\t\t\t\tAge\t\t\t\tInterests{Color.RESET}\n")
@@ -36,18 +33,15 @@ class RecordFile:
                 record = Record(fields[1], fields[2], fields[3])
                 records.append(record)
         return records
-    
     def write_record(self, record_count, record):
         with open(self.file_path, "a") as writeRecord:
             writeRecord.write(f"{record_count}\t\t\t{record.name}\t\t\t\t{record.age}\t\t\t\t{record.interests}\n")
-
 class RecordManager:
     def __init__(self):
         file_path = os.path.join("record.txt")
         self.record_file = RecordFile(file_path)
         self.records = self.record_file.read_records()
         self.record_count = len(self.records)
-    
     def add_record(self):
         name = input("Enter your name: ")
         age = input("Enter your age: ")
@@ -56,7 +50,6 @@ class RecordManager:
         self.record_count += 1
         self.records.append(record)
         self.record_file.write_record(self.record_count, record)
-    
     def view_records(self):
         if len(self.records) == 0:
             print(f"{Color.YELLOW}There are no records{Color.RESET}")
@@ -88,29 +81,31 @@ class RecordManager:
             for line in f:
                 if name in line:
                     print(line)
-manager = RecordManager()
-
-while True:            
-    print(f"{Color.CYAN}=========================================================={Color.RESET}")
-    print(f"{Color.CYAN} ~ ----------------- Record Manager --------------------- ~{Color.RESET}")
-    print(f"{Color.CYAN}==========================================================={Color.RESET}")
-    print("Available Options:")
-    print(f"A) {Color.GREEN}Add Record{Color.RESET}\tB) {Color.YELLOW}View Record{Color.RESET}")
-    print(f"C) {Color.RED}Delete Record{Color.RESET}\tD) {Color.MAGENTA}Clear Records{Color.RESET}")
-    print(f"E) {Color.WHITE}Exit Manager{Color.RESET}\tF) {Color.BLUE}Find Records{Color.RESET} \n")
-
-    selection = input('Enter selection: ').upper()
-    if selection == 'A':
-        manager.add_record()
-    elif selection == 'B':
-        manager.view_records()
-    elif selection == 'C':
-        manager.delete_record()
-    elif selection == 'D':
-        manager.clear_records()
-    elif selection == 'F':
-        manager.find_records()
-    elif selection == 'E':
-        sys.exit()
-    else:
-        print("Invalid selection. Please try again.")
+    def get_option(self):
+        print(f"{Color.CYAN}=========================================================={Color.RESET}")
+        print(f"{Color.CYAN} ~ ----------------- Record Manager --------------------- ~{Color.RESET}")
+        print(f"{Color.CYAN}==========================================================={Color.RESET}")
+        print("Available Options:")
+        print(f"A) {Color.GREEN}Add Record{Color.RESET}\tB) {Color.YELLOW}View Record{Color.RESET}")
+        print(f"C) {Color.RED}Delete Record{Color.RESET}\tD) {Color.MAGENTA}Clear Records{Color.RESET}")
+        print(f"E) {Color.WHITE}Exit Manager{Color.RESET}\tF) {Color.BLUE}Find Records{Color.RESET}")
+        option = input("Select an option: ").upper()
+        return option
+if __name__ == "__main__":
+    manager = RecordManager()
+    while True:            
+        option = manager.get_option()
+        if option == "A":
+            manager.add_record()
+        elif option == "B":
+            manager.view_records()
+        elif option == "C":
+            manager.delete_record()
+        elif option == "D":
+            manager.clear_records()
+        elif option == "E":
+            sys.exit(0)
+        elif option == "F":
+            manager.find_records()
+        else:
+            print("Invalid selection. Please try again.")
